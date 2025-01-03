332232a44d09f96d8b64b02421c9492c7790d78e
Rowan <Rowan@DESKTOP-2AGST60>
2025-01-03 21:21:45 +0100

    [CHANGE] changing some random bs.

---
+++ Code/CalculatorRowan.cs
@@ -0,0 +1,81 @@
+﻿using static CylindersCalculator.UiMethods.ConsoleUIMethods;
+
+namespace CylindersCalculator
+{
+  internal class CalculatorRowan
+  {
+    public static void CalculatorRowanMain()
+    {
+      do
+      {
+        int radius = GetPositiveInt("Define the radius of the cylinder in cm: ");
+        int height = GetPositiveInt("Define the height of the cylinder in cm: ");
+
+        double volume = CalculateVolume(radius, height);
+        double surfaceArea = CalculateSurfaceArea(radius, height);
+
+        DisplayResults(radius, height, volume, surfaceArea);
+
+      }
+      while (AskToContinue());
+    }
+
+    private static int GetPositiveInt(string prompt)
+    {
+      Console.WriteLine(prompt);
+      while (true)
+      {
+        string? input = Console.ReadLine();
+        if (int.TryParse(input, out int value) && value > 0)
+        {
+          return value;
+        }
+        else 
+        {
+          Console.Clear();
+          Console.WriteLine(prompt);
+          Console.WriteLine("Please enter a valid positive integer.");
+          Console.SetCursorPosition(0, Console.CursorTop);
+        }
+      }
+    }
+
+    private static double CalculateVolume(int radius, int height)
+    {
+      return Math.PI * Math.Pow(radius, 2) * height;
+    }
+
+    private static double CalculateSurfaceArea(int radius, int height)
+    {
+      return 2 * Math.PI * radius * (radius + height);
+    }
+
+    private static void DisplayResults(int radius, int height, double volume, double surfaceArea)
+    {
+      Console.WriteLine($"The cylinder has a radius of {radius} cm and a height of {height} cm.");
+      Console.WriteLine($"The surface area of the cylinder is {surfaceArea} cm².");
+      Console.WriteLine($"The volume of the cylinder is {volume} cm³.");
+    }
+
+    private static bool AskToContinue()
+    {
+      while (true)
+      {
+        Console.WriteLine("Do you want to define a new cylinder? Type 'y' for 'yes' or 'n' for 'no' and press Enter.");
+        char input = Console.ReadKey().KeyChar;
+
+        if (input == 'y')
+        {
+          Console.Clear();
+          return true;
+        }
+        else if (input == 'n')
+        {
+          return false;
+        }
+        RepositionCursurTopAndClearLine();
+        Console.WriteLine("You entered the wrong answer! Please answer with 'y' for 'yes' or 'n' for 'no'.");
+      }
+    }
+  }
+}
---
+++ Code/MainProgram.cs
@@ -0,0 +1,46 @@
+﻿using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+
+namespace CylindersCalculator
+{
+  internal class MainProgram
+  {
+    private static readonly Dictionary<int, (string, Action)> _actions = new()
+    {
+      {1, new (nameof(CylindersCalculatorClass), CylindersCalculatorClass.CylindersCalculatorClassMain)},
+      {2, new (nameof(CalculatorRowan), CalculatorRowan.CalculatorRowanMain)}
+    };   
+
+    private static void Main()
+    {
+      Console.WriteLine("Which program do you want to run?");
+      foreach (var action in _actions)
+      {
+        Console.WriteLine(action.Key + ". " + action.Value.Item1);
+      }
+
+      _actions[CheckInput()].Item2.Invoke();
+    }
+
+    private static int CheckInput()
+    {
+      while (true)
+      {
+        string? answer = Console.ReadKey().KeyChar.ToString();
+        if (int.TryParse(answer, out int evaluatedAnswer) && evaluatedAnswer <= _actions.Count && evaluatedAnswer > 0)
+        {
+          Console.Clear();
+          return evaluatedAnswer;
+        }
+        else
+        {
+          Console.SetCursorPosition(0, Console.CursorTop);
+          Console.Write(new string(' ', Console.WindowWidth));
+          Console.SetCursorPosition(0, Console.CursorTop);
+          Console.Write("Wrong input. Please specify the program that you want to run: ");
+        }
+      }
+    }
+  }
+}
