6d8097dd97765e3321bc844a90183057f730d442
Rowan <Rowan@DESKTOP-2AGST60>
2025-01-03 15:58:04 +0100

    [CHANGE] calculator rework. (Rowan)

--- Code/Cylinders-Calculator.cs
+++ Code/Cylinders-Calculator.cs
@@ -1,111 +1,78 @@
-﻿internal class CylindersCalculator
+﻿using System;
+
+internal class CylindersCalculator
 {
     private static void Main(string[] args)
     {
-        int r;
-        int h;
-        double Volume;
-        double SurfaceArea;
-        int UserAnswer;
-        string? input;
-
-    Start:
-        FirstInput();
-        Calculations();
-        Output();
-    BackToInput:
-        UserInput();
-        switch (EndHandler())
+        do
         {
-            case 0:
-                break;
-            case 1:
-                goto Start;
-            case 2:
-                goto BackToInput;
+            int radius = GetPositiveInt("Define the radius of the cylinder in cm: ");
+            int height = GetPositiveInt("Define the height of the cylinder in cm: ");
+
+            double volume = CalculateVolume(radius, height);
+            double surfaceArea = CalculateSurfaceArea(radius, height);
+
+            DisplayResults(radius, height, volume, surfaceArea);
+
+        } 
+        while (AskToContinue());
+    }
+
+    private static int GetPositiveInt(string prompt)
+    {
+        bool firstAttempt = true;
+        Console.Write(prompt);
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            if (int.TryParse(input, out int value) && value > 0)
+            {
+                return value;
+            }
+
+            if (firstAttempt)
+            {
+                Console.WriteLine("Please enter a valid positive integer.");
+                firstAttempt = false;
+            }
         }
+    }
 
-        void FirstInput()
+    private static double CalculateVolume(int radius, int height)
+    {
+        return Math.PI * Math.Pow(radius, 2) * height;
+    }
+
+    private static double CalculateSurfaceArea(int radius, int height)
+    {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+
+    private static void DisplayResults(int radius, int height, double volume, double surfaceArea)
+    {
+        Console.WriteLine($"The cylinder has a radius of {radius} cm and a height of {height} cm.");
+        Console.WriteLine($"The surface area of the cylinder is {surfaceArea} cm².");
+        Console.WriteLine($"The volume of the cylinder is {volume} cm³.");
+    }
+
+    private static bool AskToContinue()
+    {
+        while (true)
         {
-        Return1:
-            Console.Write("Define the radius of the cylinder in cm: ");
-            input = Console.ReadLine();
-            if (input == null | !int.TryParse(input, out int _))
+            Console.WriteLine("Do you want to define a new cylinder? Type 'y' for 'yes' or 'n' for 'no' and press Enter.");
+            string input = Console.ReadLine() ?? string.Empty;
+
+            if (input == "y")
             {
                 Console.Clear();
-                Console.WriteLine("Please enter a valid value.");
-                goto Return1;
+                return true;
             }
-            r = Convert.ToInt32(input);
-
-            bool looped = false;
-        Return2:
-            Console.Write("Define the height of the cylinder in cm: ");
-            input = Console.ReadLine();
-            if (input == null | !int.TryParse(input, out int _))
+            else if (input == "n")
             {
-                int currentLineCursor = Console.CursorTop;
-                Console.SetCursorPosition(0, currentLineCursor - 2);
-                if (looped == true)
-                {
-                    Console.SetCursorPosition(0, currentLineCursor - 1);
-                    Console.Write(new string(' ', Console.WindowWidth));
-                    Console.SetCursorPosition(0, currentLineCursor - 1);
-                }
-                else
-                {
-                    Console.SetCursorPosition(0, currentLineCursor - 1);
-                    Console.Write(new string(' ', Console.WindowWidth));
-                    Console.SetCursorPosition(0, currentLineCursor - 1);
-                    Console.WriteLine("Please enter a valid value.");
-                    Console.SetCursorPosition(0, currentLineCursor);
-                    looped = true;
-                }
-                goto Return2;
+                return false;
             }
-            h = Convert.ToInt32(input);
-        }
 
-        void Calculations()
-        {
-            Volume = Math.PI * r * r * h;
-            SurfaceArea = 2 * Math.PI * r * (r + h);
-        }
-
-        void Output()
-        {
-            Console.WriteLine($"The cylinder has an radius of {r} cm and height of {h} cm.");
-            Console.WriteLine($"The surface area of the cylinder is {SurfaceArea} cm².");
-            Console.WriteLine($"The volume of the cylinder is {Volume} cm³.");
-            Console.WriteLine();
-            Console.WriteLine("Do you want to define a new cylinder? Type 'y' for 'yes' or 'n' for 'no'.");
-        }
-
-        void UserInput()
-        {
-            UserAnswer = Convert.ToInt32(Console.ReadKey().Key);
-        }
-
-        int EndHandler()
-        {
-            switch (UserAnswer)
-            {
-                case 89:
-                    Console.Clear();
-                    return 1;
-
-                case 78:
-                    return 0;
-
-                default:
-                    Console.SetCursorPosition(0, Console.CursorTop);
-                    Console.Write(new string(' ', Console.WindowWidth));
-                    Console.SetCursorPosition(0, Console.CursorTop - 1);
-                    Console.Write(new string(' ', Console.WindowWidth));
-                    Console.SetCursorPosition(0, Console.CursorTop);
-                    Console.WriteLine("You entered the wrong answer! Please answer with 'y' for 'yes' or 'n' for 'no'.");
-                    return 2;
-            }
+            Console.WriteLine("You entered the wrong answer! Please answer with 'y' for 'yes' or 'n' for 'no'.");
         }
     }
 }
